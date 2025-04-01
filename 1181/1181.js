d1a56da829db3d6c444c3c7d1bc4d0a135d2d731
sangjin149 <sangjin149@gmail.com>
2025-04-02 00:18:47 +0900

    1181

--- 1181/1181.js
+++ 1181/1181.js
@@ -1,10 +1,15 @@
-function solution() {}
+function solution(arr) {
+    const words = [...arr];
+    words.shift();
+    const uniqueWords = [...new Set(words)];
+    const result = uniqueWords.sort((str1, str2) => str1.length - str2.length || str1.localeCompare(str2)).join("\n");
+    console.log(result);
+}
 
 const input = require("fs")
     .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
     .toString()
     .trim()
-    .split("\n")
-    .map();
+    .split("\n");
 
-solution(...input);
+solution(input);
