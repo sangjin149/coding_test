dbfbda65a028e4765c634020ac8da7432bcb03df
sangjin149 <sangjin149@gmail.com>
2025-04-09 23:58:21 +0900

    1920

---
+++ 1920/1920.js
@@ -0,0 +1,35 @@
+function binarySearch(item, arr) {
+    let head = 0;
+    let tail = arr.length - 1;
+    let result = 0;
+    do {
+        let middle = Math.floor((head + tail) / 2);
+        if (arr[middle] === item) {
+            result = 1;
+            break;
+        }
+        if (arr[middle] > item) {
+            tail = middle - 1;
+        } else {
+            head = middle + 1;
+        }
+    } while (head <= tail);
+    return result;
+}
+
+function solution(input) {
+    const inspectList = input[1].split(" ").map(Number).sort();
+    const candidateList = input[3].split(" ").map(Number);
+    const answers = [];
+    candidateList.forEach((item) => answers.push(binarySearch(item, inspectList)));
+    const result = answers.join("\n");
+    console.log(result);
+}
+
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+    .toString()
+    .trim()
+    .split("\n");
+
+solution(input);
