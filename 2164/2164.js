793116394cc260fbd00980f29593a94d7aadf26a
sangjin149 <sangjin149@gmail.com>
2025-04-11 23:52:09 +0900

    2164  고침

--- 2164/2164.js
+++ 2164/2164.js
@@ -1,10 +1,71 @@
-function solution() {}
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.size++;
+            return;
+        }
+
+        let current = this.head;
+        while (current.next) current = current.next;
+        current.next = newNode;
+        this.size++;
+    }
+
+    trial() {
+        if (this.size === 1) return this.head.value;
+        this.append(this.head.next.value);
+        this.head = this.head.next.next;
+        this.size--;
+        return 0;
+    }
+
+    print() {
+        let current = this.head;
+        let arr = [];
+        while (current) {
+            arr.push(current.value);
+        }
+        console.log(arr.join(" "));
+    }
+}
+
+function solution(input) {
+    const list = new LinkedList();
+    let i = 1;
+
+    while (i < input) {
+        list.append(i++);
+    }
+
+    list.print();
+
+    // let result = 0;
+
+    // while (result === 0) {
+    //     result = list.trial();
+    // }
+
+    // console.log(result);
+}
 
 const input = require("fs")
     .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
     .toString()
-    .trim()
-    .split("\n")
-    .map();
+    .trim();
 
-solution(input);
+solution(Number(input));
