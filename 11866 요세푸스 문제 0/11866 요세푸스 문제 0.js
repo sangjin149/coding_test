359624e76c34c0bb7846a926e138ad25b40fe125
sangjin149 <sangjin149@gmail.com>
2025-04-14 23:55:51 +0900

    11866 요세푸스 문제 0

---
+++ 11866 요세푸스 문제 0/11866 요세푸스 문제 0.js
@@ -0,0 +1,65 @@
+class Node {
+    constructor(value) {
+        this.data = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class CircularLinkdedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        if (this.size === 0) {
+            this.head = newNode;
+        } else {
+            this.tail.next = newNode;
+        }
+        newNode.next = this.head;
+        newNode.prev = this.tail;
+        this.tail = newNode;
+        this.size++;
+    }
+
+    delete(node) {
+        if (this.size === 0) return -1;
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        this.size--;
+    }
+
+    deleteOrder(K) {
+        const result = [];
+        let currentNode = this.head;
+        while (this.size > 0) {
+            for (let i = 1; i < K; i++) {
+                currentNode = currentNode.next;
+            }
+            result.push(currentNode.data);
+            this.delete(currentNode);
+            currentNode = currentNode.next;
+        }
+        console.log(`<${result.join(", ")}>`);
+    }
+}
+
+function solution(input) {
+    const [N, K] = input.split(" ").map(Number);
+    const list = new Array(N).fill(0).map((_, idx) => idx + 1);
+    const cirLink = new CircularLinkdedList();
+
+    list.forEach((value) => cirLink.append(value));
+    cirLink.deleteOrder(K);
+}
+
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+    .toString()
+    .trim();
+
+solution(input);
