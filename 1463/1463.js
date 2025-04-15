9454125bc510fa68f729e652395292110430ac1a
sangjin149 <sangjin149@gmail.com>
2025-04-15 23:54:30 +0900

    1463

--- 1463/1463.js
+++ 1463/1463.js
@@ -1,17 +1,17 @@
 function solution(input) {
-    const answerList = new Array(1000001);
-    answerList[0] = -1;
+    const answerList = new Array(1000001).fill(0);
     const target = Number(input);
 
-    for (let i = 1; i <= input; i++) {
+    for (let i = 2; i <= target; i++) {
         let minCalculation = 0;
-        if (i % 3 === 0) {
-            minCalculation = Math.min(answerList[i - 1] + 1, answerList[i / 3] + 1);
-        } else if (i % 2 === 0) {
-            minCalculation = Math.min(answerList[i - 1] + 1, answerList[i / 2] + 1);
-        } else {
-            minCalculation = answerList[i - 1] + 1;
+        minCalculation = answerList[i - 1] + 1;
+        if (i % 2 === 0) {
+            minCalculation = Math.min(minCalculation, answerList[i / 2] + 1);
         }
+        if (i % 3 === 0) {
+            minCalculation = Math.min(minCalculation, answerList[i / 3] + 1);
+        }
+
         answerList[i] = minCalculation;
     }
 
