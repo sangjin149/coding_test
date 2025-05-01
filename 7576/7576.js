973c4e0594dd186923eb400451aebeb433a9ebac
sangjin149 <sangjin149@gmail.com>
2025-05-01 23:36:15 +0900

    7576 수정

--- 7576/7576.js
+++ 7576/7576.js
@@ -1,7 +1,6 @@
 function solution(input) {
     const [M, N] = input[0];
     const graph = input.slice(1);
-    const visited = [...graph].map((row) => [...row].fill(false));
 
     const startList = [];
     let tomatoNum = M * N;
@@ -11,23 +10,11 @@ function solution(input) {
             switch (graph[i][j]) {
                 case -1:
                     tomatoNum--;
-                    visited[i][j] = true;
                     break;
                 case 1:
                     startList.push([i, j]);
-                    visited[i][j] = true;
             }
         }
-    }
-
-    function adjacent(location) {
-        const [i, j] = location;
-        const result = [];
-        if (i > 0) result.push([i - 1, j]);
-        if (j > 0) result.push([i, j - 1]);
-        if (i < N - 1) result.push([i + 1, j]);
-        if (j < M - 1) result.push([i, j + 1]);
-        return result;
     }
 
     const queue = [...startList];
@@ -35,20 +22,26 @@ function solution(input) {
     let maxDay = 0;
     let head = 0;
 
+    const dx = [-1, 1, 0, 0];
+    const dy = [0, 0, -1, 1];
+
     while (queue.length > head) {
         const curLocation = queue[head++];
         const curDayCount = graph[curLocation[0]][curLocation[1]];
-        for (const neighbor of adjacent(curLocation)) {
-            if (graph[neighbor[0]][neighbor[1]] === 0) {
+
+        for (let d = 0; d < 4; d++) {
+            const ni = curLocation[0] + dx[d];
+            const nj = curLocation[1] + dy[d];
+            if (ni >= 0 && ni < N && nj >= 0 && nj < M && graph[ni][nj] === 0) {
                 ripeCount++;
-                graph[neighbor[0]][neighbor[1]] = curDayCount + 1;
-                queue.push(neighbor);
-                maxDay = Math.max(maxDay, curDayCount + 1);
+                graph[ni][nj] = curDayCount + 1;
+                queue.push([ni, nj]);
+                maxDay = Math.max(maxDay, curDayCount);
             }
         }
     }
 
-    const result = tomatoNum === ripeCount ? Math.max(0, maxDay - 1) : -1;
+    const result = tomatoNum === ripeCount ? maxDay : -1;
     console.log(result);
 }
 
