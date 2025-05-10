28c91881fe8e4734a2f9a745139117c94ff9cbaf
sangjin149 <sangjin149@gmail.com>
2025-05-10 23:10:35 +0900

    15654

--- 15654/template.js
+++ 15654/template.js
@@ -1,10 +1,35 @@
-function solution() {}
+function solution(input) {
+    const [nm, nums] = input;
+    const [n, m] = nm;
+    const arr = nums.sort((a, b) => a - b);
+
+    const visited = new Array(n).fill(false);
+    const result = [];
+
+    function dfs(path) {
+        if (path.length === m) {
+            result.push(path.join(" "));
+            return;
+        }
+
+        for (let i = 0; i < n; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            dfs([...path, arr[i]]);
+            visited[i] = false;
+        }
+    }
+
+    dfs([]);
+
+    console.log(result.join("\n"));
+}
 
 const input = require("fs")
     .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
     .toString()
     .trim()
     .split("\n")
-    .map((str) => str.trim());
+    .map((str) => str.trim().split(" ").map(Number));
 
 solution(input);
