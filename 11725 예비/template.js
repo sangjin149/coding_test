33dfc6bd403ed5b8dfbf35761f954fe06f4f3211
sangjin149 <sangjin149@gmail.com>
2025-05-22 23:54:31 +0900

    11725

---
+++ 11725 예비/template.js
@@ -0,0 +1,10 @@
+function solution(input) {}
+
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+    .toString()
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+solution(input);
