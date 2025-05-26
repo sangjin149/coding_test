5d3e354ac4a635e8a7aa09f0010b072bb2d8f1b7
sangjin149 <sangjin149@gmail.com>
2025-05-26 23:48:29 +0900

    11053

--- 11053/template.js
+++ 11053/template.js
@@ -1,4 +1,8 @@
-function solution() {}
+function solution(input) {
+    const N = Number(input[0]);
+    const numArr = input[1].split(" ").map(Number);
+    console.log(numArr);
+}
 
 const input = require("fs")
     .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
