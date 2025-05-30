7f5e7bfe0a2b9d9c858d4145d1ad1ca4f0eef508
sangjin149 <sangjin149@gmail.com>
2025-05-30 23:57:41 +0900

    27866

---
+++ 27866/template.js
@@ -0,0 +1,12 @@
+function solution(input) {
+    console.log(input[0][Number(input[1]) - 1]);
+}
+
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+    .toString()
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+solution(input);
